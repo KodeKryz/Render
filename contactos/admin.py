6fc72ef816b64b5abc3104fd48ff4dc7bafadcb9
KodeKryz <crissaraos87@gmail.com>
2025-11-09 01:28:21 -0300

    Mejora en admin.py

--- contactos/admin.py
+++ contactos/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Contacto
+from django.http import HttpResponse
+import csv
+
+@admin.action(description='Exportar contactos seleccionados a CSV')
+def exportar_contactos_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="contactos.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Nombre', 'Teléfono', 'Correo', 'Fecha creación', 'Fecha actualización'])
+
+    for contacto in queryset:
+        writer.writerow([
+            contacto.nombre,
+            contacto.telefono,
+            contacto.correo,
+            contacto.fecha_creacion.strftime('%Y-%m-%d %H:%M'),
+            contacto.fecha_actualizacion.strftime('%Y-%m-%d %H:%M')
+        ])
+    return response
 
 @admin.register(Contacto)
 class ContactoAdmin(admin.ModelAdmin):
@@ -7,3 +27,4 @@ class ContactoAdmin(admin.ModelAdmin):
     search_fields = ('nombre', 'correo', 'telefono')
     list_filter = ('fecha_creacion',)
     ordering = ('nombre',)
+    actions = [exportar_contactos_csv]
